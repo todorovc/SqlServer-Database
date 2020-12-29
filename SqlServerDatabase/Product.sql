5e0db5201031b4dd14c418927e0ea1f9d89073f3
Christo Todorov <todorovc@gmail.com>
2020-12-29 16:06:58 +0100

    Update Product.sql

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -1,10 +1,4 @@
-﻿CREATE TABLE [dbo].[Product]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NULL, 
-    [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
-)
+
 CREATE TABLE [dbo].[Testova]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
